ce77923a1ba055e03156be7e1cceb281b4edb55b
Alexander Groß <agross@therightstuff.de>
2013-12-09 12:24:18 +0100

    Installing php modules for apache

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,5 +5,9 @@ date > /etc/vagrant-provisioned-at
 
 apt-get update
 apt-get install -y apache2
+apt-get install -y php5
+apt-get install -y libapache2-mod-php5
+/etc/init.d/apache2 restart
+
 rm -rf /var/www
 ln -fs /vagrant/src /var/www
